210e9a1926b7848627988d646b386dbf8f8f18ec
Alex Pyvovarov <goldkeeper35@gmail.com>
2018-10-25 01:00:07 +0300

    palindrom done!

--- js/app.js
+++ js/app.js
@@ -1,45 +1,45 @@
-(()=>{
+(() => {
     'use strict';
-    function fib(n) {////////    4 Task
-        let masFib=[0,1];
-        for (let i=2;i<n+1;i++){
 
-         /*   if (masFib.length===1){
-                masFib.push(1);
-            continue;
-            }*/
-            masFib[i]=masFib[i-1]+masFib[i-2];
+    function fib(n) {////////    4 Task
+        let masFib = [0, 1];
+        for (let i = 2; i < n + 1; i++) {
+
+            masFib[i] = masFib[i - 1] + masFib[i - 2];
 
         }
         console.log(masFib[n]);
     }
+
     fib(20);
 
-//////////////////////////////////
+    console.log("////////////////////////////////");
 
 
-function factorial(n) {
-    if ( n===0){
-        return 1;
+
+    function factorial(n) {
+        if (n === 0) {
+            return 1;
+        }
+        if (n !== 1) {
+            return n * factorial(n - 1);
+        }
+        return n;
     }
-   if(n!==1) {
-       return n * factorial(n - 1);
-   }
-   return n;
-}
-console.log(factorial(5));
 
-////////////////////////////////////////
+    console.log(factorial(5));
+
+    console.log("////////////////////////////////");
 
 
 
-    function isPrime(n){
-        if(n===1) // 1 - не простое число
+    function isPrime(n) {
+        if (n === 1) // 1 - не простое число
             return false;
         // перебираем возможные делители от 2 до sqrt(n)
-        for(let d=2; d*d<=n; d++){
+        for (let d = 2; d * d <= n; d++) {
             // если разделилось нацело, то составное
-            if(n%d===0)
+            if (n % d === 0)
 
                 return false;
 
@@ -49,24 +49,24 @@ console.log(factorial(5));
     }
 
     console.log(isPrime(15552
-
     ));
 
 
-    /////////////////////////
+    console.log("////////////////////////////////");
+
 
     function compareNumeric(a, b) {
         if (a > b) return 1;
         if (a < b) return -1;
     }
+
     function isSorted(mas) {
 
 
         let sortedMas = mas.slice(0);
 
         sortedMas.sort(compareNumeric);
-        console.log(sortedMas);
-        console.log(mas);
+
         for (let i = 0; i < sortedMas.length; i++) {
             if (sortedMas[i] !== mas[i]) {
                 return false;
@@ -75,12 +75,14 @@ console.log(factorial(5));
         }
         return true;
     }
+
     console.log(isSorted([3, 9, -3, 10]));
 
-    //////////////////////////////
+    console.log("////////////////////////////////");
 
-    let filter = function(arr, callback, thisArg) {
-      let  results = [];
+
+    let filter = function (arr, callback, thisArg) {
+        let results = [];
         for (let i = 0; i < arr.length; i++) {
             if (callback.call(thisArg, arr[i], i, arr)) {
                 results.push(arr[i]);
@@ -88,39 +90,93 @@ console.log(factorial(5));
         }
         return results;
     };
-    let strs = [1,2,3,4,5,0,-2,-3];
+    let strs = [1, 2, 3, 4, 5, 0, -2, -3];
 
-    let data = filter(strs,(function(element) {
-        return element > 4 ;
+    let data = filter(strs, (function (element) {
+        return element > 4;
 
     }));
     data.sort(compareNumeric);
 
     console.log(data);
-    /////////////////////////////
+
+    console.log("////////////////////////////////");
 
 
-        function reduce(mas,func,accum) {
-let result=accum;
 
-          for(let i=0;i<mas.length;i++){
-                result =func.call(null,result,mas[i],i,mas);
-                console.log(result);            }
+    function reduce(mas, func, accum) {
+        let result = accum;
 
-    return result;
+        for (let i = 0; i < mas.length; i++) {
+            result = func.call(null, result, mas[i], i, mas);
+
         }
-        console.log(reduce([1,2,3,4,5], function(result, num) {
-            return result + num;
-        }, 0));
 
-        ////////////////////////////////
+        return result;
+    }
+
+    console.log(reduce([1, 2, 3, 4, 5], function (result, num) {
+        return result + num;
+    }, 0));
+
+    console.log("////////////////////////////////");
 
     function reverse(mas) {
-        let reverseMas=[];
-        for(let i=mas.length-1;i>=0;i--){
+        let reverseMas = [];
+        for (let i = mas.length - 1; i >= 0; i--) {
             reverseMas.push(mas[i]);
         }
         return reverseMas;
     }
-    console.log(reverse([1,2,3,4,5,6,7,8]));
+
+    console.log(reverse([1, 2, 3, 4, 5, 6, 7, 8]));
+
+    console.log("////////////////////////////////");
+
+    function indexOf(mas,n) {
+        let index;
+        for(let i=0;i<mas.length;i++){
+          if ( mas[i]===n){
+         index=i;
+          }
+            }
+        return index;
+
+    }
+    console.log(indexOf([1,2,10,4,5,6],10));
+
+    console.log("////////////////////////////////");
+
+function isPalindrom(str) {
+
+    let result='';
+    let palinMas;
+    if(str===''){
+        result='yes';
+        return result;
+    }
+
+palinMas=str.split(" ").join("").toLowerCase();
+
+for(let i=0;i<palinMas.length;i++){
+
+
+    if(palinMas[i]===palinMas[palinMas.length-1-i]){
+        result='yes';
+    }
+    else{
+        result='no';
+        return result;
+    }
+
+}
+
+    return result;
+}
+
+console.log(isPalindrom('A man a plan a canal Panama'));
+
+    console.log("////////////////////////////////");
+
+
 })();
